92e0aef29a31e5733403afcece7dbf143f9f4216
dani-rz22 <00047025@uca.edu.sv>
2025-05-08 10:04:25 -0600

    se creo ejemplo del ciclo for, pero el compilador de iostream no esta funcional :(

---
+++ ejercicio.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Se muestran las opciones
+    cout << "Bienvenido, ¿Que desea hacer hoy?" << endl;
+    cout << "1. Sumar" << endl;
+    cout << "2. Restar" << endl;
+    cout << "3. Multiplicar" << endl;
+    cout << "4. Dividir" << endl;
+}
---
+++ for_ejemplo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+    int n = 10; //dato quemado 
+
+    //Bloque del ciclo for
+    //1. Inicializador del contador y declaracion de variable 
+    //2. Poner la condicional, mientras sea verdadera se dara vuelta, al llegar al falso terminara
+    //3. Seguir la cuenta en el contador de uno en uno 
+    for(int i = 1; i <= n; i++){
+        cout << 1; 
+    }
+
+
+
+    return 0; 
+}
