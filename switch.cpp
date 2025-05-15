c48410ee54767ef7db6c3e66e72313b67d117cf9
sofircds <00175525@uca.edu.sv>
2025-05-15 09:53:27 -0600

    ejercicios coon while

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+//Ejercicio utilizando while y switch
+int main(void){
+    
+    int op; 
+
+    //variable tipo bool unicamente puede representar dos valores (true, false)
+    //bool flag = true; 
+    while(true){
+        cin >> op; 
+        if(op == -1){
+            break; 
+        }
+    }
+
+}
