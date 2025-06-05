c0f299eeab9279feb59688cdbe01ef85a5ed296e
dani-rz22 <00047025@uca.edu.sv>
2025-06-05 10:29:53 -0600

    for ejemplo terminado

--- for_multiplicar.cpp
+++ for_multiplicar.cpp
@@ -4,7 +4,16 @@ using namespace std;
 int main(){
 
 
-    //Mostrar las tablas de multiplicar del 1 al 10 dle numero que le usuario ingrese 
+    //Mostrar las tablas de multiplicar del 1 al 10 del numero que el usuario ingrese 
+    int numero=0, multi; 
+
+    cout << "Ingrese un numero para mostrar sus tabla de multiplicar" << endl; 
+    cin >> numero; 
+
+    for ( int i = 1; i <= 10; i++){
+        multi = i * numero; 
+        cout << multi << endl; 
+    }
 
 
 
