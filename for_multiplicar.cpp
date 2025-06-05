456174b1358e907afc37db69bf742286df667f6e
dani-rz22 <00047025@uca.edu.sv>
2025-06-05 10:42:03 -0600

    archivos de for y aray creados mas no teminados

--- for_multiplicar.cpp
+++ for_multiplicar.cpp
@@ -7,7 +7,7 @@ int main(){
     //Mostrar las tablas de multiplicar del 1 al 10 del numero que el usuario ingrese 
     int numero=0, multi; 
 
-    cout << "Ingrese un numero para mostrar sus tabla de multiplicar" << endl; 
+    cout << "Ingrese un numero para mostrar su tabla de multiplicar" << endl; 
     cin >> numero; 
 
     for ( int i = 1; i <= 10; i++){
---
+++ while_fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){
+
+    int numero =0, n; 
+
+    cout << "Ingrese un numero para mostrar los primeros terminos en la serie Fibonacci" << endl; 
+    cin >> n; 
+
+    while (n == numero)
+    {
+        /* code */
+    }
+    
+
+
+
+
+
+
+    return 0; 
+}
