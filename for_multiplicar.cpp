3fa44155dc317d8494e03542900d628fc5a9afc8
dani-rz22 <00047025@uca.edu.sv>
2025-06-05 10:20:17 -0600

    se agrego la tarifa del consumo de kWh y se muestra su equivalente

---
+++ for_multiplicar.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){
+
+
+    //Mostrar las tablas de multiplicar del 1 al 10 dle numero que le usuario ingrese 
+
+
+
+    return 0; 
+}
--- if-else19.cpp
+++ if-else19.cpp
@@ -6,22 +6,27 @@ int main()
 
     // Calcular la tarifa electrica segun el consumo en kWh
 
-    int consumo;
+    int consumo, tarifa;
+    float precio = 2.50; 
+    //tarifa = consumo * precio;
 
     cout << "Ingrese la cantidad que consume en kWh" << endl;
     cin >> consumo;
 
     if (consumo < 100)
     {
-        cout << "Su consumo se encuentra en tarifa baja, es decir menor a 100 kWh";
+        cout << "Su consumo se encuentra en tarifa baja, es decir menor a 100 kWh" << endl;
+        cout << "La tarifa de su consumo electrico es: " << consumo*precio;
     }
     else if (consumo > 100 && consumo <= 200)
     {
-        cout << "Su consumo se encuentra en tarifa media entre 100 y 200 kWh";
+        cout << "Su consumo se encuentra en tarifa media entre 100 y 200 kWh" << endl;
+        cout << "La tarifa de su consumo electrico es: " << consumo*precio;
     }
     else
     {
-        cout << "Su consumo se encuentra en tarifa alta, es decir más 200 kWh";
+        cout << "Su consumo se encuentra en tarifa alta, es decir más 200 kWh" << endl;
+        cout << "La tarifa de su consumo electrico es: " << consumo*precio;
     }
 
     return 0;
